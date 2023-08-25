ddae34a65e0cb63519b391e186285fd8957a38cc
MariPongitori <91626226+MariPongitori@users.noreply.github.com>
2023-08-25 14:09:55 -0300

    v.1

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "db-marianapongitori.ctwen6uses0t.us-east-2.rds.amazonaws.com";
+$username = "mariana";
+$password = "pongitori25";
+$database = "concessionaria";
+?>
